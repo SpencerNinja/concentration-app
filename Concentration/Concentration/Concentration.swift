fbe7d493fd35076fed857ee5efca82b86432aaf7
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2021-03-22 16:28:24 -0600

    video 3 completed

--- Concentration/Concentration/Concentration.swift
+++ Concentration/Concentration/Concentration.swift
@@ -9,25 +9,62 @@ import Foundation
 
 class Concentration {
     
-    var cards = [Card]()
+    private(set) var cards = [Card]()
     
-    func chooseCard(at index: Int) {
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        } else {
-            cards[index].isFaceUp = true
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            // look at all the cards and see if you find only one that's face up
+            // if so, return it, else return nil
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            // turn all the cards face down except the card at index newValue
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
         }
     }
     
+    func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.choosCard(at: \(index)): chosen index not in the cards")
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // Check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+            } else {
+                // either no cards or 2 cards are face up
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+//        if cards[index].isFaceUp {
+//            cards[index].isFaceUp = false
+//        } else {
+//            cards[index].isFaceUp = true
+//        }
+    }
+    
     init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)): you must have at least one pair of cards")
         for _ in 1...numberOfPairsOfCards {
             let card = Card()
             cards += [card, card]
-            // Alternate method
-            // cards.append(card)
-            // cards.append(card)
         }
-        // To Do: Shuffle the cards
+        
+        // TODO: Shuffle the cards
         
     }
     
--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -9,19 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    lazy var game: Concentration = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    private lazy var game: Concentration = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     
-    var flipCount = 0 {
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count + 1) / 2
+    }
+    
+    private(set) var flipCount = 0 {
         didSet {
             flipCountLabel.text = "Flips: \(flipCount)"
         }
     }
     
-    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet private weak var flipCountLabel: UILabel!
     
-    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet private var cardButtons: [UIButton]!
 
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction private func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.firstIndex(of: sender) {
             game.chooseCard(at: cardNumber)
@@ -31,7 +35,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func updateViewFromModel() {
+    private func updateViewFromModel() {
         for index in cardButtons.indices {
             let button = cardButtons[index]
             let card = game.cards[index]
@@ -45,19 +49,32 @@ class ViewController: UIViewController {
         }
     }
     
-    var emojiChoices = ["🦇","😱","🙀","😈","🎃", "👻", "😬", "😳", "🍬", "🍫"]
+    private var emojiChoices = ["🦇","😱","🙀","😈","🎃", "👻", "😬", "😳", "🍬", "🍫"]
     
-    var emoji = [Int:String]()
+    private var emoji = [Int:String]()
     // Alternate
     // var emoji = Dictionary<Int,String>()
     
-    func emoji(for card: Card) -> String {
-        if emoji[card.identifier] != nil {
-            return emoji[card.identifier]!
-        } else {
-            return "?"
+     private func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
         }
+        
+        return emoji[card.identifier] ?? "?"
     }
     
+    // TODO: new game button
+    // where should it be located?
 }
 
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
