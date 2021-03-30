d38a522ea8e5db98fc867298cabda8b6776dea39
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2021-03-30 16:04:55 -0600

    swipe gesture

--- PlayingCard/PlayingCard/Controller/ViewController.swift
+++ PlayingCard/PlayingCard/Controller/ViewController.swift
@@ -11,10 +11,24 @@ class ViewController: UIViewController {
 
     var deck = PlayingCardDeck()
     
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left, .right]
+            playingCardView.addGestureRecognizer(swipe)
+        }
+    }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         
         // Print debug
 //        for _ in 1...10 {
--- PlayingCard/PlayingCard/View/PlayingCardView.swift
+++ PlayingCard/PlayingCard/View/PlayingCardView.swift
@@ -203,3 +203,13 @@ extension CGPoint {
         return CGPoint(x: x+dx, y: y+dy)
     }
 }
+
+//Face Card icons Attributions to Authors
+//
+//<div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+//
+//<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+//
+//<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+//
+//<div>Icons made by <a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
